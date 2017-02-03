da1aeb48578f6e32abcc2be0479bd1603535b75c
Kinice <szp93@126.com>
2017-02-03 17:39:43 +0800

    add default logo style

--- template/src/components/Navbar.vue
+++ template/src/components/Navbar.vue
@@ -19,5 +19,23 @@
   .g-hd{
     height: 70px;
     background: #324057;
+    min-width: 875px;
+    .m-logo{
+      line-height: 70px;
+      margin-left: 70px; 
+      //替换
+      span{
+        color: #fff;
+        font-size: 2.2em;
+      }
+    }
+    .m-user{
+      .m-wel{
+
+      }
+      .m-exit{
+
+      }
+    }
   }
 </style>
